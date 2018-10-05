5442ae44a6332476c56951879623d285c6b76865
xk <402958518@qq.com>
2018-10-05 21:53:02 +0800

    read hugepage size in current OS

--- hugepage_init.sh
+++ hugepage_init.sh
@@ -11,9 +11,13 @@ fi
 #mnt dir with hugepage file system
 mount -t hugetlbfs nodev "$huge_mnt_path"
 
+#read huge page size info
+hugepagesize=`cat /proc/meminfo | grep -i hugepagesize | awk -F'[: ]+' '{print $2}'`
+unit=`cat /proc/meminfo | grep -i hugepagesize | awk -F'[: ]+' '{print $3}'`
+
 #set count of hugae page
 hugepage_nb=0
-echo "input number of hugepage you want:"
+echo "input number of hugepage you want (huge page size is ${hugepagesize}${unit}):"
 read hugepage_nb
 
 echo ${hugepage_nb} > /proc/sys/vm/nr_hugepages
