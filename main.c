93a4b2e2cc8781cc71d42d6393dab84462740193
xk <402958518@qq.com>
2018-10-07 14:52:45 +0800

    output average access time elapsed

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ uint64_t time_measure(struct mb_node *nodes, int nodes_nb, int * visit_seq, int 
     dura = ((int64_t)end.tv_sec*1000000000 + end.tv_nsec) 
            -((int64_t)begin.tv_sec*1000000000 + begin.tv_nsec);//ns
     
-    return dura;
+    return dura / len;
 }
 
 //create visit sequence, and each elem of this sequence is a id of data in nodes 
